ff3cf8d66c87eaf54833b3480d480147eb40d989
PH <ph.ph365@gmail.com>
2025-05-23 08:43:12 -0300

    Primeiro Commit

---
+++ nutri_app/src/nutri_app/core.clj
@@ -0,0 +1,7 @@
+(ns nutri-app.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ nutri_app/test/nutri_app/core_test.clj
@@ -0,0 +1,7 @@
+(ns nutri-app.core-test
+  (:require [clojure.test :refer :all]
+            [nutri-app.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
