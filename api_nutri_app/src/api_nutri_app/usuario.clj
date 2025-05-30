bc576f01c687bcedcb512367cce209bba947d16c
PH <ph.ph365@gmail.com>
2025-05-30 10:56:42 -0300

    .

--- api_nutri_app/src/api_nutri_app/usuario.clj
+++ api_nutri_app/src/api_nutri_app/usuario.clj
@@ -1,6 +1,4 @@
-(ns api-nutri-app.usuario
-  (:require [api-nutri-app.time-util :as time-util])
-  )
+(ns api-nutri-app.usuario)
 
 (def lista_usuarios (atom []))
 
@@ -13,8 +11,3 @@
 
 (defn consultar_usuarios []
   @lista_usuarios)
-
-
-;;(registrar_usuario {:id 1, :altura 176, :peso 75, :idade 23, :sexo "M", :data_cadastro (time-util/formata-data-string (java.time.LocalDateTime/now) )})
-;;(registrar_usuario {:id 2, :altura 168, :peso 59, :idade 23, :sexo "F", :data_cadastro "31/05/2025" })
-;;(println (consultar_usuarios))
