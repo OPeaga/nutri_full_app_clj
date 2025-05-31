6a92c51e5166a82eb2828769a6a46d56d9a973ca
PH <ph.ph365@gmail.com>
2025-05-31 19:51:56 -0300

    tudo ok agr

--- api_nutri_app/src/api_nutri_app/alimento.clj
+++ api_nutri_app/src/api_nutri_app/alimento.clj
@@ -1,3 +1,12 @@
 (ns api-nutri-app.alimento)
 
+(def lista_alimento (atom []))
 
+(defn registrar_alimento [alimento]
+  (swap! lista_alimento conj alimento))
+
+(defn consultar_lista []
+  @lista_alimento)
+
+(defn limpar_lista []
+  (reset! lista_alimento []))
