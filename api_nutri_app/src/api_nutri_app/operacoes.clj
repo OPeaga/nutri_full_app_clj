6776c1326f15968302bdde19e1c062a2f9f3f543
PH <ph.ph365@gmail.com>
2025-06-01 12:58:03 -0300

    Função de registrar alimentos e calorias com datas ok, incluido também função de tradução de pt para ingles

--- api_nutri_app/src/api_nutri_app/operacoes.clj
+++ api_nutri_app/src/api_nutri_app/operacoes.clj
@@ -7,22 +7,52 @@
   )
 
 (def alimento-api-key (System/getenv "CALORIE_NINJAS_API_KEY")) ;; Transformei a api-key numa variavel de ambiente no pc
+(def traducao-api-key (System/getenv "TRANSLATE_API_KEY"))
 
 (when (nil? alimento-api-key)
   (throw (Exception. "CALORIE_NINJAS_API_KEY variavel de ambiente não definida!")))
 
-(def api-url "https://api.calorieninjas.com/v1/nutrition?query=")
+(when (nil? traducao-api-key)
+  (throw (Exception. "TRANSLATE_API_KEY variavel de ambiente não definida!")))
+
+(def api-url-alimento "https://api.calorieninjas.com/v1/nutrition?query=")
+(def api-url-traducao "https://google-translate113.p.rapidapi.com/api/v1/translator/text")
+
+(defn traducao_direta_pt_en [palavra_em_pt]
+  (try
+    (let [response (http-client/post
+                     api-url-traducao
+                     {:headers      {:x-rapidapi-key  "babe4338b3msh09ac1243ab63506p1c7aa3jsn4d927b873ca0"
+                                     :x-rapidapi-host "google-translate113.p.rapidapi.com"}
+                      :form-params  {:text palavra_em_pt
+                                     :from "pt"
+                                     :to   "en"}
+                      :as           :json
+                      }
+                     )]
+      {:success true
+       :data (get-in response [:body :trans])
+       :status (:status response)}
+
+      )
+    (catch Exception e
+      {:success false
+       :error   (.getMessage e)
+       :data    nil}))
+  )
+
 
 (defn consultar_nutricao [query]
   (try
     (let [response (http-client/get
-                     (str api-url query)
+                     (str api-url-alimento query)
                      {:headers {"X-Api-Key" alimento-api-key}
                       :as      :json})]
 
       {:success true
        :data    (:body response)
        :status  (:status response)}
+
       )
     (catch Exception e
       {:success false
@@ -39,7 +69,7 @@
             (let [porcao_referencia (:serving_size_g item)
                   calorias_referencia (:calories item)
                   calorias_consumidas (* (double (/ porcao porcao_referencia) ) calorias_referencia )]
-              ;;(str porcao "g de " alimento " Total de " calorias_consumidas " cal")
+
               calorias_consumidas
               )
             "Nenhum item encontrado"
@@ -49,7 +79,7 @@
     )
   )
 
-(defn operacao_registrar_usuario [altura peso idade sexo]
+(defn registrar_usuario [altura peso idade sexo]
   (let [usuarios (usuario/consultar_usuarios)
         id_maximo (if (empty? usuarios)
                  0
@@ -63,10 +93,15 @@
                                 :sexo sexo
                                 :data_registro data_atual_formatada})))
 
-(defn operacao_registra_alimento [alimento porcao]
-  (let [calorias_consumidas (calcular_calorias_por_porcao alimento porcao)
+
+(defn registrar_alimento [alimento porcao]
+  (let [alimento_em_ingles (:data (traducao_direta_pt_en alimento))
+        calorias_consumidas (calcular_calorias_por_porcao porcao alimento_em_ingles)
         data_atual_formatada (.format (LocalDateTime/now) (DateTimeFormatter/ofPattern "dd/MM/yyyy"))]
-    (alimento/regi)
+    (alimento/registrar_alimento {:alimento (str porcao "g de " alimento) :calorias calorias_consumidas :data data_atual_formatada})
     )
   )
 
+;;(def pt "arroz")
+;;(registrar_alimento pt 150 )
+;;(println (alimento/consultar_lista))
