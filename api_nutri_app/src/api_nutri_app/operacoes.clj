358c8ebd9f08c8ae0534e85d40366cb8c3363c49
PH <ph.ph365@gmail.com>
2025-06-04 11:45:33 -0300

    Fiz quase tudo agora falta somente a parte de calcular saldo calórico por periodo

--- api_nutri_app/src/api_nutri_app/operacoes.clj
+++ api_nutri_app/src/api_nutri_app/operacoes.clj
@@ -74,7 +74,7 @@
 
               calorias_consumidas
               )
-            "Nenhum item encontrado"
+            (throw (Exception. "Nenhum item encontrado, provável tradução desconhecida"))
             )
         )
       )
@@ -95,14 +95,13 @@
                                 :sexo sexo
                                 :data_registro data_atual_formatada})))
 
-
 (defn converte_kg_pounds [peso_kg]
   (* (double peso_kg) 2.20462)
   )
 
 (defn calcular_gasto_calorico [atividade duracao]
   (let [usuario (usuario/getUsuario_por_id 1)] ;; Trabalhamos com o usuário 1
-    (if (not (nil? usuario))
+    (if usuario
       (let [peso_pounds (converte_kg_pounds (:peso usuario))]
         (try
           (let [response (http-client/get api-url-exercicio
@@ -115,22 +114,31 @@
                                            :as :json})
                 atividades (:body response)]
 
-            (if (or (nil? atividades) (empty? atividades))
+            (if
+              (empty? atividades )
               (throw (Exception. (str "Nenhuma atividade correspondente a '" atividade "' encontrada na API")))
-
-              (let [primeira-atividade (first atividades)
-                    calorias (:total_calories primeira-atividade)]
-                {:success true
-                 :data calorias
-                 :status (:status response)})
+              (do
+                (let [primeira-atividade (first atividades)
+                        calorias (:total_calories primeira-atividade)]
+                    {:success true
+                     :data calorias
+                     :status (:status response)}
+                    )
+                  )
               )
             )
 
           (catch Exception e
             {:success false
              :error (.getMessage e)
-             :data nil})))
-      "Usuário ainda não cadastrado")))
+             :data nil}
+            )
+          )
+        )
+      (throw (Exception. "Usuário ainda não cadastrado"))
+      )
+    )
+  )
 
 
 (defn registrar_alimento [alimento porcao data]
@@ -142,73 +150,117 @@
                                     :data     data})
       "Alimento Registrado com sucesso")
     (catch Exception e
-      "Erro ao registrar alimento")
+      (.getMessage e)
+      )
     )
   )
 
 (defn registrar_exercicio [atividade duracao data]
   (try
     (let [atividade_em_ingles (:data (traducao_direta_pt_en atividade))
-          calorias_perdidas (:data (calcular_gasto_calorico atividade_em_ingles duracao))] ;; verificar quais atividades vamos dispor para selecionar no front
-      (exercicio/registrar_exercicio {:exercicio (str atividade " por " duracao " minutos") :calorias (* -1.00 calorias_perdidas) :data data})
-      "Exercício Registrado com Sucesso"
+          resposta (calcular_gasto_calorico atividade_em_ingles duracao )]
+
+      (if (:success resposta)
+        (do
+          (let [calorias_perdidas (:data resposta)]
+            (exercicio/registrar_exercicio
+              {:exercicio (str atividade " por " duracao " minutos")
+               :calorias (* -1.0 (double calorias_perdidas))
+               :data data})
+            )
+          "Exercicio cadastrado com sucesso"
+          )
+        (throw (Exception. (str (:error resposta))))
+        )
       )
+
     (catch Exception e
-     (.getMessage e)
+      (str "Erro ao registrar exercício: " (.getMessage e))
+      )
     )
   )
+
+
+(defn inInterval? [data_ref data_ini data_fim]
+  (let [formatter_data (DateTimeFormatter/ofPattern "dd/MM/yyyy")]
+    (and
+      (.isBefore (LocalDate/parse data_ref formatter_data) (.plusDays (LocalDate/parse data_fim formatter_data) 1))
+      (.isAfter (LocalDate/parse data_ref formatter_data ) (.minusDays (LocalDate/parse data_ini formatter_data) 1))
+      )
+    )
   )
 
-(def lista_alimentos_temp                                   ;; Feito para se basear nas consultas de extrato sem fazer requisicoes
-  [{:alimento "150g de arroz", :calorias 191.10000000000002, :data "25/05/2025"}
-   {:alimento "200g de frango_grelhado", :calorias "Nenhum item encontrado", :data "25/05/2025"}
-   {:alimento "80g de banana", :calorias 71.52000000000001, :data "26/05/2025"}
-   {:alimento "50g de aveia", :calorias 184.15, :data "26/05/2025"}
-   {:alimento "180g de salmao", :calorias 375.65999999999997, :data "27/05/2025"}
-   {:alimento "100g de brocolis", :calorias 35.0, :data "27/05/2025"}
-   {:alimento "120g de batata_doce", :calorias 93.0, :data "28/05/2025"}
-   {:alimento "140g de ovos", :calorias 202.02, :data "28/05/2025"}
-   {:alimento "170g de iogurte_grego", :calorias "Nenhum item encontrado", :data "29/05/2025"}
-   {:alimento "90g de quinoa", :calorias 109.62, :data "29/05/2025"}
-   {:alimento "60g de espinafre", :calorias 13.98, :data "30/05/2025"}
-   {:alimento "160g de peito_peru", :calorias "Nenhum item encontrado", :data "30/05/2025"}
-   {:alimento "200g de abacate", :calorias 334.4, :data "31/05/2025"}
-   {:alimento "40g de castanha_para", :calorias "Nenhum item encontrado", :data "31/05/2025"}
-   {:alimento "70g de cenoura", :calorias 23.799999999999997, :data "01/06/2025"}
-   {:alimento "150g de atum", :calorias 199.95000000000002, :data "01/06/2025"}
-   {:alimento "110g de manga", :calorias "Nenhum item encontrado", :data "02/06/2025"}
-   {:alimento "130g de lentilha", :calorias 146.64000000000001, :data "02/06/2025"}
-   {:alimento "85g de tomate", :calorias 15.469999999999999, :data "03/06/2025"}
-   {:alimento "50g de amendoim", :calorias 289.25, :data "05/06/2025"}])
-
+(defn sortExtrato [registros]
+  (let [formatter_data (DateTimeFormatter/ofPattern "dd/MM/yyyy")]
+    (sort-by #(LocalDate/parse (:data %) formatter_data) registros)))
 
 (defn consultar_extrato
-  ([tipo data_ini data_fim])
-
-
-
-  ([data_ini data_fim]
-    (let [transacoes lista_alimentos_temp                                       ;; (conj (alimento/consultar_alimentos) (exercicio/consultar_exercicios))
-          formater_data (java.time.format.DateTimeFormatter/ofPattern "dd/MM/yyyy")]
-       (filter (fn [registro]
-                ;;(.format (LocalDate/parse (:data registro)) formater_data)
-                (and
-                  (.isBefore (LocalDate/parse (:data registro) formater_data) (.plusDays (LocalDate/parse data_fim formater_data) 1))
-                  (.isAfter (LocalDate/parse (:data registro) formater_data ) (.minusDays (LocalDate/parse data_ini formater_data) 1))
-                  )
-                ) transacoes)
-       )
-
+  ([tipo data_ini data_fim]
+   (cond
+     (= tipo 1) (sortExtrato (filter #(inInterval? (:data %) data_ini data_fim) (alimento/consultar_alimentos)))
+     (= tipo 2) (sortExtrato (filter #(inInterval? (:data %) data_ini data_fim) (exercicio/consultar_exercicios)))
+     :else "Comando desconhecido"
+     )
    )
-
+  ([data_ini data_fim]
+    (let [transacoes (concat (alimento/consultar_alimentos) (exercicio/consultar_exercicios))]
+       (sortExtrato (filter #(inInterval? (:data %) data_ini data_fim) transacoes))
+   )
+   )
   )
 
-(def consultar_extrato_exercicios (consultar_extrato partial 2)) ;;Exercicios serão do tipo 2
-(def consultar_extrato_alimentos (consultar_extrato partial 1)) ;;Alimentos serão do tipo 1
+;(defn calcular_balanco_calorico [data_ini data_fim])
+
+(def consultar_extrato_exercicios (partial consultar_extrato 2))
+(def consultar_extrato_alimentos  (partial consultar_extrato 1))
+
+(println (registrar_usuario 176 82 23 "M") )
+(println (registrar_alimento "arroz" 150 "25/05/2025"))
+(println (registrar_alimento "frango_grelhado" 200 "25/05/2025"))
+(println (registrar_alimento "banana" 80 "26/05/2025"))
+(println (registrar_alimento "aveia" 50 "26/05/2025"))
+(println (registrar_alimento "salmao" 180 "27/05/2025"))
+(println (registrar_alimento "brocolis" 100 "27/05/2025"))
+(println (registrar_alimento "batata_doce" 120 "28/05/2025"))
+(println (registrar_alimento "ovos" 140 "28/05/2025"))
+(println (registrar_alimento "iogurte_grego" 170 "29/05/2025"))
+(println (registrar_alimento "quinoa" 90 "29/05/2025"))
+(println (registrar_alimento "espinafre" 60 "30/05/2025"))
+(println (registrar_alimento "peito_peru" 160 "30/05/2025"))
+(println (registrar_alimento "abacate" 200 "31/05/2025"))
+(println (registrar_alimento "castanha_para" 40 "31/05/2025"))
+(println (registrar_alimento "cenoura" 70 "01/06/2025"))
+(println (registrar_alimento "atum" 150 "01/06/2025"))
+(println (registrar_alimento "manga" 110 "02/06/2025"))
+(println (registrar_alimento "lentilha" 130 "02/06/2025"))
+(println (registrar_alimento "tomate" 85 "03/06/2025"))
+(println (registrar_alimento "amendoim" 50 "05/06/2025"))
+(println (registrar_exercicio "corrida" 30 "25/05/2025"))
+(println (registrar_exercicio "musculacao" 45 "25/05/2025"))
+(println (registrar_exercicio "natacao" 60 "26/05/2025"))
+(println (registrar_exercicio "yoga" 40 "26/05/2025"))
+(println (registrar_exercicio "ciclismo" 90 "27/05/2025"))
+(println (registrar_exercicio "pilates" 50 "27/05/2025"))
+(println (registrar_exercicio "caminhada" 25 "28/05/2025"))
+(println (registrar_exercicio "crossfit" 35 "28/05/2025"))
+(println (registrar_exercicio "alongamento" 15 "29/05/2025"))
+(println (registrar_exercicio "boxe" 45 "29/05/2025"))
+(println (registrar_exercicio "futebol" 75 "30/05/2025"))
+(println (registrar_exercicio "tenis" 60 "30/05/2025"))
+(println (registrar_exercicio "danca" 40 "31/05/2025"))
+(println (registrar_exercicio "escalada" 80 "31/05/2025"))
+(println (registrar_exercicio "basquete" 55 "01/06/2025"))
+(println (registrar_exercicio "volei" 50 "01/06/2025"))
+(println (registrar_exercicio "spinning" 45 "02/06/2025"))
+(println (registrar_exercicio "jiu_jitsu" 70 "02/06/2025"))
+(println (registrar_exercicio "hidroginastica" 35 "03/06/2025"))
+(println (registrar_exercicio "skate" 40 "05/06/2025"))
 
 
 
 
-
-;(println (alimento/consultar_alimentos))
 (println (consultar_extrato "29/05/2025" "03/06/2025"))
+(println "-----------------------------\n")
+(println (consultar_extrato_exercicios "29/05/2025" "03/06/2025"))
+(println "-----------------------------\n")
+(print (consultar_extrato_alimentos "29/05/2025" "03/06/2025"))
