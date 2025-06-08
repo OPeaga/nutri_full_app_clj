5087b54bc59f3a8be21c907f329de08531dc4365
PH <ph.ph365@gmail.com>
2025-06-08 18:12:37 -0300

    Iniciei o front, falta ajustar o menu de selecionar usuários que deve trazer os dados de um usuário

--- app_nutri/project.clj
+++ app_nutri/project.clj
@@ -6,7 +6,8 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                   [clj-http	"3.9.1"]
 								  [org.clojure/tools.cli	"0.4.1"]
-                                  ]
+                  [cheshire "5.12.0"]
+                 ]
   :main ^:skip-aot app-nutri.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- app_nutri/src/app_nutri/core.clj
+++ app_nutri/src/app_nutri/core.clj
@@ -1,9 +1,63 @@
 (ns app-nutri.core
-  (:require [clj-http.client :as	http-client])
+  (:require [clj-http.client :as	http-client]
+            [cheshire.core :as json]
+            )
   (:gen-class))
+
+(def url-server "http://localhost:3000")
+
+(defn menu_selecionar_usuario []
+  (let [response (http-client/get (str url-server "/usuario/consulta"))]
+    (if (= (:status response) 200)
+      (let [body (:body response)
+            altura (:altura body)
+            peso (:peso body)
+            idade (:idade body)
+            sexo (:sexo body)
+            data-registro (:data_registro body)]
+        (println body)
+        (println (str "Altura: " altura
+                      ", Peso: " peso
+                      ", Idade: " idade
+                      ", Sexo: " sexo
+                      ", Data de Registro: " data-registro)))
+      (println "Erro: não foi possível consultar o usuário."))))
+
+
+(defn menu_registrar_usuario []
+  (println "Digite : (altura em cm) (peso em kg) (idade) (M para Masculino e F para Feminino)")
+  (let [altura (read)
+        peso (read)
+        idade (read)
+        sexo (read)
+        json-data (json/generate-string {:altura altura :peso peso :idade idade :sexo sexo})]
+    (http-client/post (str url-server "/usuario/cadastro")
+                      {:body json-data
+                       :headers {"Content-Type" "application/json"}
+                       :content-type :json
+                       :accept :json})
+    )
+  )
+
+(defn menu_usuario [entrada_param]
+  (println "Digite uma opção : \n1.Registrar Novo Usuario\n2.Selecionar Usuário\n3.Encerrar Aplicativo")
+  (if (= 3 entrada_param)
+    nil
+    (do
+      (let [entrada (read)]
+        (cond
+          (= 1 entrada) (do (menu_registrar_usuario) (recur entrada) )
+          (= 2 entrada) (do (menu_selecionar_usuario) (recur entrada) )
+          )
+        )
+      )
+    )
+  )
+
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!")
+  (println "Bem-vindos ao App Nutri ---------------------------------")
+  (menu_usuario 0)
   )
